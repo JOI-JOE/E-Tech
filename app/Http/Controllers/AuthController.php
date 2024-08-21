82a02ba18d9e7059f09f1e0243f7d6627cabf768
JOI-JOE <iknowhtml161gmail.com>
2024-08-21 18:39:49 +0700

    done Cart + Checkout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('front.account.login');
+    }
+
+    public  function register()
+    {
+        return view('front.account.register');
+    }
+
+    public function processRegister(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+           'name' => 'required|min:3',
+           'email' => 'required|email|unique:users',
+           'password' => 'required|min:5|confirmed'
+        ]);
+
+        if($validator->passes()){
+
+            $user = new User;
+            $user->name = $request->name;
+            $user->phone = $request->phone;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            session()->flash('success','You have been registered successfully.');
+
+            return response()->json([
+                'status' => true,
+            ]);
+        }else{
+            return response()->json([
+               'status' => false,
+               'errors' => $validator->errors(),
+            ]);
+        }
+    }
+
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+           'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if($validator->passes()){
+
+            if(Auth::attempt([
+                'email' => $request->email,
+                'password' => $request->password,
+            ], $request->get('remember'))){
+
+                if(session()->has('url.intended')){
+                    return redirect(session()->get('url.intended'));
+                }
+                return redirect()->route('account.profile');
+
+            }else{
+//                session()->flash('error','Either email/password is incorrect');
+                return redirect()->route('account.login')
+                    ->with($request->only('email'))
+                    ->with('error','Either email/password is incorrect');
+            }
+
+        }else{
+            return redirect()->route('account.login')
+                ->withErrors($validator)
+                ->withInput($request->only('email'));
+        }
+    }
+
+    public function profile()
+    {
+        return view('front.account.profile');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('account.login')
+            ->with('success','You successfully logged out');
+    }
+}
